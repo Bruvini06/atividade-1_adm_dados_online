ce96789a39d6d10b4d3264998ab514fd681edbcb
bruno almeida <brunoviniciusa06@gmail.com>
2026-08-18 21:01:48 -0300

    testa a analise

--- analise.py
+++ analise.py
@@ -4,9 +4,9 @@ import pandas as pd
 # Substitua 'seu_arquivo.csv' pelo caminho ou nome do seu arquivo
 df = pd.read_csv('Libertadores.csv')
 
-# 2. Exibir as primeiras 5 linhas do DataFrame
-print("--- Primeiras 5 linhas ---")
-print(df.head())
+# 2. Exibir as primeiras 10 linhas do DataFrame
+print("--- Primeiras 10 linhas ---")
+print(df.head(10))
 
 # 3. Exibir informações gerais sobre o conjunto de dados (colunas, tipos e valores nulos)
 print("\n--- Informações do DataFrame ---")
